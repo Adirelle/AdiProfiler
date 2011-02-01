d328e00e7f34a1790c6144c14c8671da7bf3713a
Adirelle <adirelle@tagada-team.net>
2011-02-01 17:28:02 +0100

    Do not display functions that were never called and limit the output to 15 entries per category.

--- AdiProfiler.lua
+++ AdiProfiler.lua
@@ -58,21 +58,24 @@ local function AddData(tooltip, items, fetchFunc, total)
 	for ref, name in pairs(items) do
 		local usage, numCalls = fetchFunc(ref, false)
 		local totalUsage, totatNumCalls = fetchFunc(ref, true)
-		if not usages[name] then
-			tinsert(names, name)
-			usages[name] = usage
-			totalUsages[name] = totalUsage
-			calls[name] = numCalls
-			totalCalls[name] = totatNumCalls
-		else
-			usages[name] = usages[name] + usage
-			totalUsages[name] = totalUsages[name] + totalUsage
-			calls[name] = calls[name] + numCalls
-			totalCalls[name] = totalCalls[name] + totatNumCalls
+		if numCalls > 0 or totatNumCalls > 0 then
+			if not usages[name] then
+				tinsert(names, name)
+				usages[name] = usage
+				totalUsages[name] = totalUsage
+				calls[name] = numCalls
+				totalCalls[name] = totatNumCalls
+			else
+				usages[name] = usages[name] + usage
+				totalUsages[name] = totalUsages[name] + totalUsage
+				calls[name] = calls[name] + numCalls
+				totalCalls[name] = totalCalls[name] + totatNumCalls
+			end
 		end
 	end
 	table.sort(names, SortByTotalUsage)
-	for i, name in ipairs(names) do
+	for i = 1, min(#names, 15) do
+		local name = names[i]
 		tooltip:AddDoubleLine(name, format("|cffffffff%d:|r %.3f%% (|cffffffff%d:|r %.3f%%)", 
 			totalCalls[name],
 			totalUsages[name] / total * 100,
