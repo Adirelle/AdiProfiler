c03fc36015d24c6de36648a584090c9c8f019961
Adirelle <adirelle@tagada-team.net>
2011-02-02 13:32:55 +0100

    Sort by time/call instead of total time.

--- AdiProfiler.lua
+++ AdiProfiler.lua
@@ -40,48 +40,44 @@ function addon:RegisterFrame(frame, name)
 end
 
 local names = {}
-local usages = {}
-local totalUsages = {}
+local usage = {}
 local calls = {}
+local totalUsage = {}
 local totalCalls = {}
 
-local function SortByTotalUsage(a, b)
-	return totalUsages[b] < totalUsages[a]
+local function Sort(a, b)
+	return (usage[b] / calls[b]) < (usage[a] / calls[a])
 end
 
 local function AddData(tooltip, items, fetchFunc, total)
 	wipe(names)
-	wipe(usages)
-	wipe(totalUsages)
+	wipe(usage)
+	wipe(totalUsage)
 	wipe(calls)
 	wipe(totalCalls)
 	for ref, name in pairs(items) do
-		local usage, numCalls = fetchFunc(ref, false)
-		local totalUsage, totatNumCalls = fetchFunc(ref, true)
+		local refUsage, numCalls = fetchFunc(ref, false)
+		local refTotalUsage, totatNumCalls = fetchFunc(ref, true)
 		if numCalls > 0 or totatNumCalls > 0 then
-			if not usages[name] then
+			if not usage[name] then
 				tinsert(names, name)
-				usages[name] = usage
-				totalUsages[name] = totalUsage
+				usage[name] = refUsage
+				totalUsage[name] = refTotalUsage
 				calls[name] = numCalls
 				totalCalls[name] = totatNumCalls
 			else
-				usages[name] = usages[name] + usage
-				totalUsages[name] = totalUsages[name] + totalUsage
+				usage[name] = usage[name] + refUsage
+				totalUsage[name] = totalUsage[name] + refTotalUsage
 				calls[name] = calls[name] + numCalls
 				totalCalls[name] = totalCalls[name] + totatNumCalls
 			end
 		end
 	end
-	table.sort(names, SortByTotalUsage)
+	table.sort(names, Sort)
 	for i = 1, min(#names, 15) do
 		local name = names[i]
-		tooltip:AddDoubleLine(name, format("|cffffffff%d:|r %.3f%% (|cffffffff%d:|r %.3f%%)", 
-			totalCalls[name],
-			totalUsages[name] / total * 100,
-			calls[name],
-			usages[name] / total * 100
-		))
+		local calls, usage = calls[name], usage[name]
+		tooltip:AddDoubleLine(name, format("%d || %.1f || %.3f",  calls, usage, usage / calls))
 	end
 end
 
@@ -96,11 +92,13 @@ function dataobj.OnTooltipShow(tooltip)
 		AddData(tooltip, functions, GetFunctionCPUUsage, GetScriptCPUUsage())
 	end
 	
+	--[[
 	if next(frames) then
 		local total = GetFrameCPUUsage(UIParent, "true")
 		tooltip:AddLine("Frames", 1, 1, 1)
 		AddData(tooltip, frames, GetFrameCPUUsage, total)
 	end
-	
+	--]]
+
 	tooltip:Show()
 end
